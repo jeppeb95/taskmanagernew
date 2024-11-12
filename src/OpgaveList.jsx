50fa99e981484d11091dbd2b722d013979cd91b5
Jeppe <jeppe.birkholm.andersen@gmail.com>
2024-11-12 11:38:50 +0100

    Mobil optimering, rettelser af farver, opdatering af font og tilføjelse af symbol

--- src/OpgaveList.jsx
+++ src/OpgaveList.jsx
@@ -13,17 +13,23 @@ function OpgaveList({ tasks, onClearTasks }) {
                 <li>Proritet</li>
             </ul>
         </div>
-            <ul>
-                {tasks.map((task, index) => (
-                    <ul className="task-ul" key={index}>
-                        <li className='task-title'>{task.taskTitle}</li>
-                        <li>{task.dueDate}</li>
-                        <li className='type-li'><div className='type-box'>{task.taskType}</div></li>
-                        <li>{task.priority}</li>
-                        <li className='task-description'>{task.taskDescription}</li>
-                    </ul>
-                ))}
-            </ul>
+            <ul class="tasks-list">
+    {tasks.map((task, index) => (
+        <li className="task-ul" key={index}>
+            <div className="task-title">{task.taskTitle}</div>
+            <div className='date-class'><span><img src="svg/date-symbol.svg" alt="Piktogram af en kalender og ur" /></span>{task.dueDate}</div>
+            <div className='type-prio-wrapper'>
+            <div className='type-container'><div className="type-box">{task.taskType}</div></div>
+            <div className='priority-container'><div className={`priority-class ${task.priority.toLowerCase()}`}>
+    {task.priority}
+</div>
+</div>
+            </div>
+            <div className="task-description">{task.taskDescription}</div>
+        </li>
+    ))}
+</ul>
+
             <button className="clearButton" onClick={onClearTasks}>
                 Ryd opgaveliste
             </button>
